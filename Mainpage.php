80d3fa3b6c15cd4304ae2ab04b328960296056b2
dyhae <oulabi>
2019-04-03 13:43:35 -0500

    Main page

---
+++ Mainpage.php
@@ -0,0 +1,51 @@
+<?php
+// session_start();
+require("template.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="stylepage.css">
+    <script src="main.js"></script>
+</head>
+<header>
+        <div id="headerdiv">
+             <h1>Cozy Dinner</h1>
+             <p>
+             AT Cozy dinner, you will be served hearty traditional favorites along with delectable 
+             italian dessert and pastry specialist.In nice weather, 
+             you can enjoy all of these trats beneath the chesnut tree in the garden dinning area
+         </p>
+         <a class="readmore" href="about.php">Read More</a>
+        </div>
+    </header>
+<body id="mainbody">
+    
+    <div id="middleBody">
+                <h1>Welcome!</h1>
+                <p>May you have a good experience with us.Enjoy!!</p>
+                     <img src="images/cosy.jpg" alt="fuck">
+
+        <div id="display">
+            <div>
+                    <h1>Find your Favorites</h1>
+                    <p>Go to the menu and get your favorite foods</p>
+            </div>
+            <div>
+                    <h1>Best Seller</h1>
+                    <p>We are the best in the area, with the best price and quality</p>
+            </div>
+            <div>
+                <h1>Reservation</h1>
+                <p>Make a reservation a day before.</p>
+            </div>
+        </div>
+   
+    </div>
+</body>
+</html>
