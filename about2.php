f38b4e958dea9154137f51475816d82d77e0e37b
dyhae <oulabi>
2019-04-12 15:29:54 -0500

    abbout2.php

---
+++ about2.php
@@ -0,0 +1,23 @@
+<?php
+// session_start();
+require("template.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="stylepage.css">
+    <script src="main.js"></script>
+</head>
+<body id="aboutBody">
+    <h1>Cozy Dinner.</h1>
+    <h1>Phone: (204)577-1284.</h1>
+    <h1>Address: 224 ColdFairy Street.</h1>
+    <h1>Mission: is to insure the satisfaction of our customers and provide the best service.</h1>
+    <h1>Cirtified by : The goverment of Canada.</h1>
+</body>
+</html>
